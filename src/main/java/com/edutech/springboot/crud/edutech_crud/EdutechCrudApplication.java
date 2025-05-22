f9e49e6d0bbba14d88c81aa2122e5c68a7f2939c
twinkgenocide <illusivelove@proton.me>
2025-05-22 15:24:18 -0400

    Inicialización de Proyecto

---
+++ src/main/java/com/edutech/springboot/crud/edutech_crud/EdutechCrudApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.springboot.crud.edutech_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutechCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edutech/springboot/crud/edutech_crud/EdutechCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.springboot.crud.edutech_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutechCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
