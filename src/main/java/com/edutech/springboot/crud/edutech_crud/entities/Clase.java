8cf0bb521d99e8e9c15ecc294bf21634169c6823
viriq <vi.riquelmea@duocuc.cl>
2025-05-24 23:40:44 -0400

    Agregado entidad 'Clase'

---
+++ src/main/java/com/edutech/springboot/crud/edutech_crud/entities/Clase.java
@@ -0,0 +1,66 @@
+package com.edutech.springboot.crud.edutech_crud.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="clase")
+public class Clase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String descripción;
+
+    @Column(name = "nombre_instructor")
+    private String nombreInstructor;
+
+    public Clase(Long id, String titulo, String descripción, String nombreInstructor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripción = descripción;
+        this.nombreInstructor = nombreInstructor;
+    }
+
+    public Clase() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripción() {
+        return descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
+    }
+
+    public String getNombreInstructor() {
+        return nombreInstructor;
+    }
+
+    public void setNombreInstructor(String nombreInstructor) {
+        this.nombreInstructor = nombreInstructor;
+    }
+
+}
