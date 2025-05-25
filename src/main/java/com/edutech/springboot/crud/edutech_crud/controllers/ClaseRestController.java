010578db9387e5ba2597998c671d2021160603ae
viriq <vi.riquelmea@duocuc.cl>
2025-05-25 18:59:38 -0400

    Implementado métodos de escritura en ClaseRestController

--- src/main/java/com/edutech/springboot/crud/edutech_crud/controllers/ClaseRestController.java
+++ src/main/java/com/edutech/springboot/crud/edutech_crud/controllers/ClaseRestController.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +35,29 @@ public class ClaseRestController {
         if (claseOptional.isPresent()) {
             return ResponseEntity.ok(claseOptional.orElseThrow());
         }
+        return ResponseEntity.notFound().build();
+    }
+    
+    @PostMapping
+    public ResponseEntity<Clase> crear(@RequestBody Clase clase) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(claseService.save(clase));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> modificar(@PathVariable long id, @RequestBody Clase clase) {
+        Optional<Clase> claseOptional = claseService.findById(id);
+
+        if (claseOptional.isPresent()) {
+            Clase claseExistente = claseOptional.get();
+
+            claseExistente.setTitulo(clase.getTitulo());
+            claseExistente.setDescripción(clase.getDescripción());
+            claseExistente.setNombreInstructor(clase.getNombreInstructor());
+
+            Clase claseModificada = claseService.save(claseExistente);
+            return ResponseEntity.ok(claseModificada);
+        }
+
         return ResponseEntity.notFound().build();
     }
 
