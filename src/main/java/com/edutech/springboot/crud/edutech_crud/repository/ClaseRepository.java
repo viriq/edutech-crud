93d80765ead8b5bddb461c55abd07b2bf371a3b5
viriq <vi.riquelmea@duocuc.cl>
2025-05-24 23:48:44 -0400

    Agregado Repositorio de 'Clase'

---
+++ src/main/java/com/edutech/springboot/crud/edutech_crud/repository/ClaseRepository.java
@@ -0,0 +1,9 @@
+package com.edutech.springboot.crud.edutech_crud.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.edutech.springboot.crud.edutech_crud.entities.Clase;
+
+public interface ClaseRepository extends CrudRepository<Long, Clase> {
+
+}
