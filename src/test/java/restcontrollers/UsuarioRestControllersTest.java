8df8d107e2464f7e61b27141811cfdcb6f739f8b
furanko222 <f.pisanimontenegro@gmail.com>
2025-06-21 22:01:39 -0400

    se crea metodo service.findByAll

--- src/test/java/restcontrollers/UsuarioRestControllersTest.java
+++ src/test/java/restcontrollers/UsuarioRestControllersTest.java
@@ -1,10 +1,19 @@
 package restcontrollers;
 
+import static org.mockito.Mockito.when;
+
 import java.util.List;
 
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,7 +38,17 @@ public class UsuarioRestControllersTest {
 
     private List<Usuario> usuarioLista;  
 
+    @Test
+    public void verUsuariosTest() throws Exception{
+        when(usuarioserviceimpl.findByAll()).thenReturn(usuarioLista);
+        mockmvc.perform(get("/api/usuarios")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+
     
+
 
 
 
