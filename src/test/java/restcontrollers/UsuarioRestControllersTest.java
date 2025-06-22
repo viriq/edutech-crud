461371376c33df1c0cc8170d395de868b230f44e
furanko222 <f.pisanimontenegro@gmail.com>
2025-06-21 22:30:15 -0400

    se ordenan imports

--- src/test/java/restcontrollers/UsuarioRestControllersTest.java
+++ src/test/java/restcontrollers/UsuarioRestControllersTest.java
@@ -1,7 +1,6 @@
 package restcontrollers;
 
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -12,6 +11,7 @@ import javax.print.attribute.standard.Media;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.ArgumentMatchers.any;
 
 
 
