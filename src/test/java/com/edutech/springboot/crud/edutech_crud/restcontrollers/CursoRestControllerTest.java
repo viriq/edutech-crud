418d4ce71e6626d23d360e5bdb24928f8c57fb58
viriq <vi.riquelmea@duocuc.cl>
2025-06-23 01:54:02 -0400

    Agregado Test de Integración 'modificarNotFound' para CursoRestController

--- src/test/java/com/edutech/springboot/crud/edutech_crud/restcontrollers/CursoRestControllerTest.java
+++ src/test/java/com/edutech/springboot/crud/edutech_crud/restcontrollers/CursoRestControllerTest.java
@@ -5,6 +5,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
@@ -46,19 +49,13 @@ public class CursoRestControllerTest {
     }
 
     @Test
-    public void verCursoTest() {
+    public void verCursoTest() throws Exception {
         Curso unCurso = new Curso(73L, "Ciberseguridad Básica", "Curso básico de ciberseguridad y protección de datos personales.", "Fernando Salinas");
-        try {
-            when(cursoServiceImpl.findById(73L)).thenReturn(Optional.of(unCurso));
-            mockMvc.perform(get("/api/cursos/73")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-        }
-        catch(Exception ex) {
-            fail("Test 'verCursoTest' lanzó un error: " + ex.getMessage());
-        }
+        when(cursoServiceImpl.findById(73L)).thenReturn(Optional.of(unCurso));
+        mockMvc.perform(get("/api/cursos/73")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
     }
-
 
     @Test
     public void cursoNoExisteTest() throws Exception {
@@ -69,7 +66,7 @@ public class CursoRestControllerTest {
     }
 
     @Test
-    public void cursoSaveTest() throws Exception {
+    public void cursoCrearTest() throws Exception {
         Curso inputCurso = new Curso(null, "Arte y Código", "Taller de programación creativa con Python y arte generativo.", "Martín Reyes");
         Curso outputCurso = new Curso(45L, inputCurso.getTitulo(), inputCurso.getDescripción(), inputCurso.getNombreInstructor());
         when(cursoServiceImpl.save(any(Curso.class))).thenReturn(outputCurso);
@@ -77,6 +74,40 @@ public class CursoRestControllerTest {
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(inputCurso)))
             .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void cursoModificarTest() throws Exception {
+        Long id = 45L;
+        Curso existingCurso = new Curso(id, "Arte y Código", "Taller de programación creativa con Python y arte generativo.", "Martín Reyes");
+        Curso inputCurso = new Curso(null, existingCurso.getTitulo(), existingCurso.getDescripción(), "Richard Ericson");
+        Curso outputCurso = new Curso(id, inputCurso.getTitulo(), inputCurso.getDescripción(), inputCurso.getNombreInstructor());
+
+        when(cursoServiceImpl.findById(id)).thenReturn(Optional.of(existingCurso));
+        when(cursoServiceImpl.save(any(Curso.class))).thenReturn(outputCurso);
+
+        mockMvc.perform(put("/api/cursos/" + id)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(inputCurso)))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.id").value(45L))
+            .andExpect(jsonPath("$.titulo").value(existingCurso.getTitulo()))
+            .andExpect(jsonPath("$.nombreInstructor").value(inputCurso.getNombreInstructor()));
+
+    }
+
+    @Test
+    public void cursoModificarNotFoundTest() throws Exception {
+        Long id = 97L;
+        Curso inputCurso = new Curso(null, "Arte y Código", "Taller de programación creativa con Python y arte generativo.", "Richard Ericson");
+
+        when(cursoServiceImpl.findById(id)).thenReturn(Optional.empty());
+
+        mockMvc.perform(put("/api/cursos/" + id)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(inputCurso)))
+            .andExpect(status().isNotFound());
     }
 
 }
